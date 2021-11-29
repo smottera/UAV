9bd7d5c2bb090e652c6bb94254b264846a86c28b
Sam Mottera <samanthms131211@gmail.com>
2021-11-29 23:07:27 +0530

    Update OpenCVTut.py

--- OpenCVTut.py
+++ OpenCVTut.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import matplotlib
 
 def contourDetector():
 	# Reading image
@@ -78,12 +79,12 @@ cv2.resize(img, (780, 540), interpolation = cv2.INTER_NEAREST)
 # Get number of pixel horizontally and vertically.
 (height, width) = img.shape[:2]
 
-#cv2.imshow("Cute Kitens", img)
+cv2.imshow("Cute Kitens", img)
 
 # Specify the size of image along with interploation methods.
 # cv2.INTER_AREA is used for shrinking, whereas cv2.INTER_CUBIC
 # is used for zooming.
-#res = cv2.resize(img, (int(width / 2), int(height / 2)), interpolation = cv2.INTER_CUBIC)
+res = cv2.resize(img, (int(width / 2), int(height / 2)), interpolation = cv2.INTER_CUBIC)
 
 #cv2.imwrite('result.jpg', res)
 
@@ -128,6 +129,6 @@ cv2.imwrite('RandomGray.png', grayImage)
 
 # Make an array of 120,000 random bytes.
 #randomByteArray = bytearray(os.urandom(120000))
-#flatNumpyArray = np.array(randomByteArray)
+flatNumpyArray = np.array(randomByteArray)
 
 
