b165d89422403512b28f5df2536ce1948a76c492
samanth <samanthms131211@gmail.com>
2022-03-19 17:02:39 +0530

    Update byExample.go

--- byExample.go
+++ byExample.go
@@ -4,19 +4,11 @@ import (
     "fmt"
     "time"
 )
-
-
-
-
 func main() {
     now := time.Now()
 
-
-
     channel1 := make(chan string)
-    go func() {channel1 <- "Hello"}()
-
-
+    go func() {channel1 <- "Heello"}()
 
     fmt.Println(<-channel1)
     fmt.Println(time.Now().Sub(now))
