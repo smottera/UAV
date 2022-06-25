d601eea804eb857bed19a0fb7f1f9b76195c7760
samanth <samanthms131211@gmail.com>
2022-06-26 01:08:44 +0530

    Update main.go

--- main.go
+++ main.go
@@ -24,30 +24,62 @@ ___________________________________________________________________________
                                                                       	(images/frames need to be memcached in Redis
 ___________________________________________________________________________
 FROM TxRx.go
-Build supporting data structures, primary variables.
-Build a function that randomizes these variables for future testing purposes
-Build other 3 variants of gRPC services for testing
-Test over LAN and then internet (NGINX required?)
-
-
+1. Implement a watchdog function
+5. Implement Drone Social Network (drone ID, drone specific data, authorization)
+6. Implement Unmanned Traffic Management function with the help of the above info
+7. Implement necessary backend storage and caching services as needed
+8. Design and build a scheduler?
+9. create stubClient, stubServer, stubRepeater subPackages?
+12. test new proto file -> Test Latency and reliability -> test on SoC -> test under different network circumstances
 */
 
-// TESTS
+//setup sever-side streaming, client-side streaming and bidirectional streaming services/interfaces and test
+//compare performance. Introduce more real world tests
+//setup gateway, test across the internet
 // test with multiple server / client instances
 // test what happens to stream during connection loss
 // txrx.go should be able to heal connection after loss ASAP
-//12. test new proto file -> Test Latency and reliability -> test on SoC -> test under different network circumstances
-//5. Implement Drone Social Network (drone ID, drone specific data, authorization)
-//6. Implement Unmanned Traffic Management function with the help of the above info
-//7. Implement necessary backend storage and caching services as needed
-//8. Design and build a scheduler?
-//9. create stubClient, stubServer, stubRepeater subPackages?
+
 /*
 protoc -I ./ protofiles/person.proto --go_out=plugins=grpc:.
 protoc -I ./ protofiles/person.proto --go-grpc_out=plugins=grpc:.
 
 */
 
+//get list of of all peripherals connected
+//check wireless connections
+//check communication with HQ/cloud
+//check if flight controls are working correctly
+//check if data is correctly being logged
+//check health of all cells
+//log data
+//calculate range, lifespan, next service date
+//inputs: mission start time, deadline (time), description, distance,
+// path[[gcode]], landing bool?, payload info, attempts, failure?complete?,
+//return address
+//backup return address
+//check mission queue
+//stop motion, reset attitude
+//plan path for landing
+//begin path to decend
+//check sensors for possibilty of landing
+//continue phase 2 of decent
+//touch down
+//poweroff main motor
+//Disarm
+//inputs:starting,ending points,
+//regions to avoid, regions to pass through
+//collion prevention with other UAVs
+//path dependent on time?
+//output: path coordinates, keep track of timing
+//check ping/latency with all comms
+//measure signal strength, in proximity UAVs and HQs
+//log everything
+//check if connection with HQ is okay
+//check if sbus output to microcontroller is okay
+//in case of signal loss: continue with current path? path plan a new Return to home then land
+
+//This is the Server Stub (GQC Backend!)
 package main
 
 import (
@@ -65,13 +97,13 @@ import (
 
 const (
 	port      = ":50051"
-	noOfSteps = 3
+	noOfSteps = 12
 )
 
-//priority 0
 var (
 	uniqueID string
 
+	//priority 0
 	batteryLow   bool = true
 	safe2Land    bool = false
 	returnToHome bool = false
@@ -80,63 +112,78 @@ var (
 	homeLat      float32
 	homeLon      float32
 	homeAlt      float32
+
+	//controls
+	throttle   int = 1500 //Channel 0
+	rudder     int = 1500
+	aileron    int = 1500
+	elevator   int = 1500
+	motorPower int = 1500
+	aux1       int = 1500
+	aux2       int = 1500
+	aux3       int = 1500 //Channel 7
+
+	//telemetry
+	batteryVoltage float32 = 1.0
+	currentDraw    float32 = 2.0
+	longitude      float32 = 3.0
+	latitude       float32 = 4.0
+	altitude       float32 = 5.0
+	temperature    float32 = 40.3
+	motorRPM       float32 = 9000.23
+	gyro           float32 = 123.21
+	accel          float32 = 432.12
+
+	//mission packet
+	missionID  uint
+	mLatitude  float32
+	mLongitude float32
+	mAltitude  float32
+	repeat     bool
 )
-
-//priority 1
-type controlPacket struct {
-	throttle   int //Channel 0
-	rudder     int
-	aileron    int
-	elevator   int
-	motorPower int
-	aux1       int
-	aux2       int
-	aux3       int //Channel 7
-}
-
-//priority 2
-type missionPacket struct {
-	missionID uint
-	latitude  float32
-	longitude float32
-	altitude  float32
-	repeat    bool
-}
-
-//priority 3
-type telemetryPacket struct {
-	batteryVoltage float32
-	currentDraw    float32
-	longitude      float32
-	latitude       float32
-	altitude       float32
-	temperature    float32
-	motorRPM       int
-}
 
 func droneDummyDataGenerator(iterate int, delay int) {
 
 	for j := 0; j <= iterate; j++ {
 
-		time.Sleep(time.Millisecond * time.Duration(delay))
 		//init these variables
 
 		rand1 := float32(rand.Float64())
+		rand2 := int(rand.Int())
 		timestamp := time.Now()
+
+		//priority 0
+		batteryLow = true
 		safe2Land = false
-		disarm = true
 		returnToHome = false
+		disarm = true
+		mode = 10 * rand2
+		homeLat = 1501 * rand1
+		homeLon = 1505 * rand1
+		homeAlt = 1801 * rand1
 
-		mode = 0
-		homeLat = 123.321 * rand1
-		homeLon = 987.789 * rand1
-		homeAlt = 6900.32 * rand1
+		//Control params
+		throttle = 1500 //Channel 0
+		rudder = 1501 * rand2
+		aileron = 1501 * rand2
+		elevator = 1501 * rand2
+		motorPower = 1501 * rand2
+		aux1 = 1501 * rand2
+		aux2 = 1501 * rand2
+		aux3 = 1501 * rand2 //Channel 7
 
-		t1 := telemetryPacket{12.3 * rand1, 123.2 * rand1, 123123.3123123 * rand1, 3333.555 * rand1, 23.46, 69.4 * rand1, 9999}
-		m1 := missionPacket{123, 32.55 * rand1, 112.34 * rand1, 12.99 * rand1, false}
-		c1 := controlPacket{0, 0, 1, 2, 3, 4, 5, 6}
+		//telemetry randz
+		batteryVoltage = 1500 * rand1
+		currentDraw = 1501 * rand1
+		longitude = 1502 * rand1
+		latitude = 1503 * rand1
+		altitude = 1504 * rand1
+		temperature = 1505 * rand1
+		motorRPM = 1506 * rand1
+		gyro = 1507 * rand1
+		accel = 1508 * rand1
 
-		fmt.Println("Yoo  ", rand1, timestamp, t1, m1, c1, safe2Land, mode, disarm, returnToHome, homeAlt, homeLat, homeLon)
+		fmt.Println("Battery Voltage", batteryVoltage, currentDraw, homeLat, homeLon, delay, timestamp)
 	}
 }
 
@@ -153,86 +200,24 @@ func (s *server) GetTelemetry(in *pb.Acknowledged, stream pb.UavControl_GetTelem
 		// Simulating I/O or Computation process using sleep........
 		// Usually this will be saving money transfer details in DB or
 		// talk to the third party API
-		time.Sleep(time.Second * 1)
+		time.Sleep(time.Microsecond * 1)
 		// Once task is done, send the successful message back to the client
-		if err := stream.Send(&pb.TelemetryPacket{BatteryVoltage: 1.234, CurrentDraw: 3.2, Altitude: 33.92}); err != nil {
+		if err := stream.Send(&pb.TelemetryPacket{
+			BatteryVoltage: batteryVoltage,
+			CurrentDraw:    currentDraw,
+			Altitude:       altitude,
+			Longitude:      longitude,
+			Temperature:    temperature,
+			MotorRPM:       motorRPM,
+			Gyro:           gyro,
+			Accel:          accel}); err != nil {
+
 			log.Fatalf("%v.Send(%v) = %v", stream, "status", err)
 		}
 	}
 
 	log.Printf("Successfully transfered amount $%v ", in.A)
 	return nil
-}
-
-func initSys() {
-	fmt.Println("Hello Early Adopter. Welcome to TxRx.go")
-	batteryService()
-	missionService()
-	sensorCheck()
-	//get list of of all peripherals connected
-	//check wireless connections
-	//check communication with HQ/cloud
-	//check if flight controls are working correctly
-	//check if data is correctly being logged
-
-	fmt.Println("All Systems Go!")
-}
-
-func sensorCheck() int {
-	return 0
-}
-func batteryService() {
-	//check health of all cells
-	//log data
-	//calculate range, lifespan, next service date
-	fmt.Println("Battery status ... unknown")
-}
-
-func missionService() {
-	fmt.Println("Mission scheduler empty ...!")
-	//inputs: mission start time, deadline (time), description, distance,
-	// path[[gcode]], landing bool?, payload info, attempts, failure?complete?,
-	//return address
-	//backup return address
-	//check mission queue
-
-}
-
-func landingService() {
-	fmt.Println("Preparing for landing ...")
-	//stop motion, reset attitude
-	//plan path for landing
-	//begin path to decend
-	//check sensors for possibilty of landing
-	//continue phase 2 of decent
-	//touch down
-	//poweroff main motor
-	//Disarm
-	fmt.Println("Landing complete.")
-}
-
-func PathPlanneringProMax() {
-	fmt.Println("Path planned.")
-	//inputs:starting,ending points,
-	//regions to avoid, regions to pass through
-	//collion prevention with other UAVs
-	//path dependent on time?
-	//output: path coordinates, keep track of timing
-
-}
-
-func commsHeartBeat() {
-	//check ping/latency with all comms
-	//measure signal strength, in proximity UAVs and HQs
-	//log everything
-	fmt.Println("All comms okay.")
-
-	//check if connection with HQ is okay
-	//check if sbus output to microcontroller is okay
-	//in case of signal loss: continue with current path? path plan a new Return to home then land
-}
-func getJoystickData() {
-	fmt.Println("NIGGA PWEASE")
 }
 
 func testProtoMarshalling() {
@@ -264,13 +249,9 @@ func testProtoMarshalling() {
 	now := time.Now()
 	diff := now.Sub(then)
 	fmt.Println("Time taken: ", diff)
-
 }
-func main() {
 
-	initSys()
-	//testProtoMarshalling()
-
+func testTelemetry() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
@@ -288,4 +269,13 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
+}
+func main() {
+
+	//initSys()
+	//testProtoMarshalling()
+
+	droneDummyDataGenerator(50, 1)
+	testTelemetry()
+
 }
