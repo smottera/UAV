67381c6a365b098a4bae19ec7ccdaf6712f12732
samanth <samanthms131211@gmail.com>
2023-03-17 23:05:01 +0530

    Update dbDriver.go

--- dbDriver.go
+++ dbDriver.go
@@ -1,4 +1,4 @@
-//Redsign go services to be compatible with GCP.
+///Redsign go services to be compatible with GCP.
 //middleware between backend services and postgres (and/or redis)
 //
 //https://golangdocs.com/golang-postgresql-example
